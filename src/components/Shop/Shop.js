f5cb24237b9d12fe531345c6cb3ddaf9565730ea
faria-r <fariacse11@gmail.com>
2022-09-26 19:30:54 +0600

    Products displayed form DB

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import'./Shop.css'
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import'./Shop.css';
 const Shop = () => {
+    const [products, setproducts]=useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res =>res.json())
+        .then(data => setproducts(data))
+    },[])
     return (
         <div className="shop-container">
            <div className="products-container">
-<h3>This is for products.</h3>
+{
+    products.map(product => <Product 
+        key={product.id}
+        product={product}
+    ></Product>)
+}
            </div>
            <div className="cart-contaienr">
 <h4>This is order summery.</h4>
