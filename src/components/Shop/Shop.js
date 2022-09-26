07343deca0457a539b2892bfd2cd0947fd07fca3
faria-r <fariacse11@gmail.com>
2022-09-26 18:38:59 +0600

    Shops Product Container added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import'./Shop.css'
+const Shop = () => {
+    return (
+        <div className="shop-container">
+           <div className="products-container">
+<h3>This is for products.</h3>
+           </div>
+           <div className="cart-contaienr">
+<h4>This is order summery.</h4>
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
